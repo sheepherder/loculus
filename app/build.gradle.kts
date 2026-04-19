5294a0c3e212f41926b62f31818e47c960416299
schaefer <13237870+sheepherder@users.noreply.github.com>
2026-04-19 22:06:23 +0200

    Extract appVersion constant, set archive name to loculus

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
+val appVersion = "0.1.0"
+
+base {
+    archivesName = "loculus-$appVersion"
+}
+
 android {
     namespace = "de.schaefer.eosgps"
     compileSdk = 36
@@ -14,7 +20,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1"
+        versionName = appVersion
     }
 
     buildFeatures {
@@ -32,6 +38,7 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
 }
 
 kotlin {
